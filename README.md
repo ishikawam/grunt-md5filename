e279cfb17708f3f9c78dff6a9c9b9f77f3d963a8
Masayuki Ishikawa (M_Ishikawa) <masayuki.ishikawa@gree.net>
2013-05-28 11:59:11 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,72 @@
+# grunt-md5filename
+[![Build Status](https://travis-ci.org/ishikawam/grunt-md5filename.png?branch=master)](https://travis-ci.org/ishikawam/grunt-md5filename)
+
+> Convert file names to MD5.
+
+
+
+## Getting Started
+This plugin requires Grunt `~0.4.0`
+
+If you haven't used [Grunt](http://gruntjs.com/) before, be sure to check out the [Getting Started](http://gruntjs.com/getting-started) guide, as it explains how to create a [Gruntfile](http://gruntjs.com/sample-gruntfile) as well as install and use Grunt plugins. Once you're familiar with that process, you may install this plugin with this command:
+
+```shell
+npm install grunt-md5filename --save-dev
+```
+
+Once the plugin has been installed, it may be enabled inside your Gruntfile with this line of JavaScript:
+
+```js
+grunt.loadNpmTasks('grunt-md5filename');
+```
+
+*This plugin was designed to work with Grunt 0.4.x. If you're still using grunt v0.3.x it's strongly recommended that [you upgrade](http://gruntjs.com/upgrading-from-0.3-to-0.4), but in case you can't please use [v0.3.2](https://github.com/gruntjs/grunt-contrib-copy/tree/grunt-0.3-stable).*
+
+
+
+## MD5filename task
+_Run this task with the `grunt md5filename` command._
+
+Task targets, files and options may be specified according to the grunt [Configuring tasks](http://gruntjs.com/configuring-tasks) guide.
+### Options
+
+#### pathtype
+Type: `String`
+
+
+#### keepBasename
+Type: `String`
+
+
+#### keepExtension
+Type: `String`
+
+
+### Usage Examples
+
+```js
+md5filename: {
+  build: {
+    options: {
+      keepBasename: false, // デフォルトfalseでは(MD5ファイル名.jpg) trueでは(元のファイル名-MD5ファイル名.jpg)
+      keepExtension: true, // デフォルトfalseでは拡張子を排除 trueでは元の拡張子を付与
+      pathType: 'filename', // MD5元は filename ファイル名, filepath 相対パス
+      debug: true, // 元ファイル、保存ファイルを表示
+    },
+    expand: true, // ディレクトリ構成を保つかどうか
+    cwd: 'original/img/thumbnails/', // expand:true の場合のベースパス
+    src: ['**/*.{png,jpg}'], // 元のファイル
+    dest: 'htdocs/img/thumbnails/', // 保存先
+  },
+}
+```
+
+
+## Release History
+
+ * 2013-05-28   v0.0.1   init.
+
+---
+
+Task submitted by [Masayuki Ishikawa](https://github.com/ishikawam)
+
---
+++ tasks/grunt-md5.js
@@ -0,0 +1,97 @@
+/*
+ * grunt-md5filename
+ * https://github.com/ishikawam/grunt-md5filename
+ *
+ * Copyright (c) 2013 Masayuki Ishikawa
+ * Licensed under the MIT license.
+ */
+
+module.exports = function(grunt) {
+  'use strict';
+
+  var path = require('path');
+
+  grunt.registerMultiTask('md5filename', 'change filename md5', function() {
+
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+        keepBasename: false,
+        keepExtension: false,
+        pathType: 'filename',
+        debug: false,
+    });
+
+    var count = 0;
+
+    grunt.verbose.writeflags(options, 'Options');
+
+    this.files.forEach(function(filePair) {
+      var isExpandedPair = filePair.orig.expand || false;
+
+      filePair.src.forEach(function(srcFile) {
+        try {
+          var basename = '';
+          var destFile;
+          var ext = '';
+          var filename;
+
+          // default: use filename
+          var srcCode = path.basename(srcFile);
+          if (options.pathType === 'filepath') {
+            srcCode = srcFile;
+          }
+
+          if (options.keepBasename === true) {
+            basename = path.basename(srcFile, ext || path.extname(srcFile)) + '-';
+          }
+
+          if (options.keepExtension === true) {
+            ext = path.extname(srcFile);
+          }
+
+          filename = basename + require('crypto').createHash('md5').update(srcCode).digest('hex') + ext;
+
+          var regex = new RegExp(escapeRegExp(path.basename(srcFile)) + "$");
+
+          if (detectDestType(filePair.dest) === 'directory') {
+            destFile = (isExpandedPair) ? filePair.dest.replace(regex, filename) : unixifyPath(path.join(filePair.dest, filename));
+          } else {
+            destFile = filePair.dest.replace(regex, filename);
+          }
+          grunt.file.copy(srcFile, destFile);
+
+          if (options.debug === true) {
+            grunt.log.writeln('File \'' + srcFile + '\' to \'' + destFile + '\' created.');
+          }
+
+        } catch(err) {
+          grunt.log.error(err);
+          grunt.fail.warn('Fail to generate an MD5 file name');
+        }
+      });
+      count++;
+    });
+    grunt.log.writeln(count + ' files created.');
+  });
+
+  var detectDestType = function(dest) {
+    if (grunt.util._.endsWith(dest, '/')) {
+      return 'directory';
+    } else {
+      return 'file';
+    }
+  };
+
+  var unixifyPath = function(filepath) {
+    if (process.platform === 'win32') {
+      return filepath.replace(/\\/g, '/');
+    } else {
+      return filepath;
+    }
+  };
+
+  var escapeRegExp = function(s) {
+    return s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+  };
+};
+
